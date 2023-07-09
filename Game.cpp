f498f303375250ede3411240899e7a40a296112f
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-09 01:57:13 +0000

    added

---
+++ Game.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    string name;
+    int age;
+
+    // Names Box();
+
+    cout << "\n" << endl;
+    cout << "Name : ";
+    cin >> name;
+    cout << "your name is " << name << endl;
+    cout << "Age : ";
+    cin >> age;
+    cout << "Age => " << age;
+
+    // Box.Hello();
+}
